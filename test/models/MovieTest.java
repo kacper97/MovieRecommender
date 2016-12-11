870a0d65e78770e04753d5901577fde334210e2e
kacper woloszyn <kacperwoloszyn4@gmail.com>
2016-12-11 21:35:52 +0000

    Movie Test and Data.xml imported

--- test/models/MovieTest.java
+++ test/models/MovieTest.java
@@ -1,5 +1,32 @@
 package models;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class MovieTest {
 
+	Movie test = new Movie("CSGO", "2012", "www.csgoBlog.com");
+
+	@Test
+	public void testCreateMovie() {
+		assertEquals("CSGO", test.getTitle());
+		assertEquals("2012", test.getYear());
+		assertEquals("www.csgoBlog.com", test.getUrl());
+	}
+
+	@Test
+	public void testGetTitle() {
+		assertEquals("CSGO", test.getTitle());
+	}
+
+	@Test
+	public void testYear() {
+		assertEquals("2012", test.getYear());
+	}
+
+	@Test
+	public void testGetUrl() {
+		assertEquals("www.csgoBlog.com", test.getUrl());
+	}
 }
